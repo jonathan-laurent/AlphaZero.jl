73476bd885a857025befb45b18fd22cea4c01f52
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2019-10-19 08:28:06 -0400

    Update inference benchmark

--- profile/inference.jl
+++ profile/inference.jl
@@ -1,7 +1,14 @@
+#####
+##### Profile neural network inference speed
+#####
+
 using Plots
 using Statistics
 
-using CuArrays
+using CUDAapi
+if has_cuda()
+  using CuArrays
+end
 using Flux
 
 import AlphaZero; const AZ = AlphaZero
@@ -9,15 +16,15 @@ import AlphaZero.GI
 
 include("../games/tictactoe/game.jl")
 import .TicTacToe
-const Game = TicTacToe.Game
+Game = TicTacToe.Game
 
 FIG_FILE = "inference-speedup.png"
 REP  = 100
 NMAX = 64
 
-netparams = AlphaZero.SimpleNetParams(
+netparams = AlphaZero.SimpleNetHyperParams(
   width=500,
-  depth_common=7)
+  depth_common=4)
 
 function measure(network, X, A)
   t = @elapsed begin
@@ -28,7 +35,7 @@ function measure(network, X, A)
   return t/REP
 end
 
-network = AlphaZero.SimpleNet{Game, netparams}()
+network = AlphaZero.SimpleNet{Game}(netparams)
 game = Game()
 board = GI.board(game)
 actions = GI.available_actions(game)
@@ -57,18 +64,23 @@ end
 # Compile everything
 single()
 batch(2)
-batch_gpu(2)
-
+println("Profiling inference on individual boards...") ; flush(stdout)
 tsingle = single()
+println("Profiling inference on batches...") ; flush(stdout)
 tbatches = [batch(n) for n in 1:NMAX]
-tbatches_gpu = [batch_gpu(n) for n in 1:NMAX]
 
 speedup_curve(ts) = [(i * tsingle) / t for (i, t) in enumerate(ts)]
 
 title="Inference speedup as a funtion of batch size"
-hline([1], label="Baseline", title=title)
+hline([1], label="Baseline",
+  title=title, xlabel="Batch size", ylabel="Speedup")
 plot!(speedup_curve(tbatches), label="CPU")
-plot!(speedup_curve(tbatches_gpu), label="GPU")
-xlabel!("Batch size")
-ylabel!("Speedup")
+
+if has_cuda()
+  batch_gpu(2)
+  tbatches_gpu = [batch_gpu(n) for n in 1:NMAX]
+  plot!(speedup_curve(tbatches_gpu), label="GPU")
+end
+
+println("Generated: $FIG_FILE")
 savefig(FIG_FILE)
