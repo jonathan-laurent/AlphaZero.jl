40d4607ce349e88140a6ec194ff8132f41187ff2
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2019-10-23 13:02:56 -0400

    Try new parameters for Mancala

--- games/mancala/params.jl
+++ games/mancala/params.jl
@@ -6,30 +6,36 @@ netparams = AlphaZero.SimpleNetHyperParams(
   depth_vbranch=2,
   depth_pbranch=2)
 
-mcts = AlphaZero.MctsParams(
-  num_workers=32,
-  use_gpu=true,
-  num_iters_per_turn=640,
-  dirichlet_noise_ϵ=0)
-
 self_play = AlphaZero.SelfPlayParams(
   num_games=600,
   reset_mcts_every=200,
-  mcts=mcts)
+  mcts=AlphaZero.MctsParams(
+    use_gpu=true,
+    num_workers=32,
+    num_iters_per_turn=640,
+    cpuct=4,
+    temperature=1,
+    dirichlet_noise_ϵ=0))
 
 # Exploration is induced by MCTS and by the temperature τ=1
 # TODO: change arena: simpler MCTS, less iterations
 arena = AlphaZero.ArenaParams(
-  num_games=200,
-  reset_mcts_every=40,
+  num_games=1000,
+  reset_mcts_every=1,
   update_threshold=(2 * 0.52 - 1),
-  mcts=mcts)
+  mcts=AlphaZero.MctsParams(
+    use_gpu=true,
+    num_workers=16,
+    num_iters_per_turn=160,
+    cpuct=4,
+    temperature=0.3,
+    dirichlet_noise_ϵ=0.05))
 
 learning = AlphaZero.LearningParams(
   learning_rate=1e-3,
-  l2_regularization=1e-5,
+  l2_regularization=5e-6,
   nonvalidity_penalty=1.,
-  checkpoints=[2, 5, 20, 60])
+  checkpoints=[2, 4, 6, 12])
 
 params = AlphaZero.Params(
   arena=arena,
@@ -44,4 +50,10 @@ validation = AlphaZero.RolloutsValidation(
   baseline=AlphaZero.MctsParams(
     num_iters_per_turn=1000,
     dirichlet_noise_ϵ=0),
-  contender=mcts)
+  contender=AlphaZero.MctsParams(
+    use_gpu=true,
+    num_workers=32,
+    num_iters_per_turn=640,
+    cpuct=4,
+    temperature=0.3,
+    dirichlet_noise_ϵ=0))
