99b1b63ae775a5aeeaa88052d8bb0d1438dddd93
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2019-11-18 15:41:27 -0500

    Update connect four params

--- games/connect-four/params.jl
+++ games/connect-four/params.jl
@@ -3,12 +3,12 @@ Network = ResNet{Game}
 cold_temperature = 0.2
 
 netparams = ResNetHP(
-  num_filters=64,
+  num_filters=128,
   num_blocks=5,
-  conv_kernel_size=(3,3),
+  conv_kernel_size=(3, 3),
   num_policy_head_filters=4,
   num_value_head_filters=32,
-  batch_norm_momentum=0.3)
+  batch_norm_momentum=0.05)
 
 self_play = SelfPlayParams(
   num_games=3_000,
@@ -33,9 +33,9 @@ arena = ArenaParams(
     dirichlet_noise_ϵ=0.05))
 
 learning = LearningParams(
-  batch_size=256,
-  loss_computation_batch_size=1024,
-  gc_every=2_000,
+  batch_size=64,
+  loss_computation_batch_size=64,
+  gc_every=0,
   learning_rate=1e-3,
   l2_regularization=1e-4,
   nonvalidity_penalty=1.,
@@ -48,7 +48,7 @@ params = Params(
   num_iters=40,
   num_game_stages=5,
   mem_buffer_size=PLSchedule(
-    [      0,       20],
+    [      0,       40],
     [200_000, 2_000_000]))
 
 validation = nothing
