08810c45b29fe996195bba7150a34588bea6511f
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2019-11-25 17:37:35 -0500

    Enable pretty urls in doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,12 @@
 using Documenter
 using AlphaZero
 
+const PRETTY_URLS = get(ENV, "CI", nothing) == "true"
+
 makedocs(
     sitename = "AlphaZero",
     authors="Jonathan Laurent",
-    format = Documenter.HTML(prettyurls=false),
+    format = Documenter.HTML(prettyurls=PRETTY_URLS),
     modules = [AlphaZero],
     pages = [
         "index.md",
@@ -13,7 +15,7 @@ makedocs(
         "mcts.md",
         "misc.md"
     ],
-    repo="github.com/jonathan-laurent/AlphaZero.jl/blob/{commit}{path}#L{line}"
+    repo="https://github.com/jonathan-laurent/AlphaZero.jl/blob/{commit}{path}#L{line}"
 )
 
 deploydocs(;
