3cc910f85f351e263224a11e420e3061b45e4d90
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2019-12-05 22:14:31 -0500

    Add a unit test that runs a debug tictactoe session

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using AlphaZero
 using Test
 
+include("../scripts/game_module.jl")
+@game_module TicTacToe "tictactoe"
+using .TicTacToe: Game, Training
+
 @testset "AlphaZero.jl" begin
-    # Write your own tests here.
+  @test begin
+    Net, netparams, params, benchmark = Training.get_params(:debug)
+    session = Session(
+      Game, Net, params, netparams,
+      dir=Training.SESSION_DIR, autosave=false, benchmark=benchmark)
+    resume!(session)
+    return true
+  end
 end
