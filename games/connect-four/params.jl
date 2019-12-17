d0720f21a10ae87b4091d877fd9323a420abd971
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2019-12-17 14:19:05 -0500

    Slight update of the connect-four params

--- games/connect-four/params.jl
+++ games/connect-four/params.jl
@@ -13,7 +13,7 @@ netparams = ResNetHP(
   batch_norm_momentum=0.3)
 
 self_play = SelfPlayParams(
-  num_games=(DEBUG ? 1 : 4_000),
+  num_games=(DEBUG ? 1 : 6_000),
   reset_mcts_every=1_000,
   gc_every=nothing,
   mcts=MctsParams(
@@ -30,7 +30,7 @@ self_play = SelfPlayParams(
 arena = ArenaParams(
   num_games=(DEBUG ? 1 : 400),
   reset_mcts_every=400,
-  update_threshold=(2 * 0.55 - 1),
+  update_threshold=(2 * 0.54 - 1),
   mcts=MctsParams(self_play.mcts,
     temperature=StepSchedule(cold_temperature),
     dirichlet_noise_ϵ=0.05))
@@ -39,7 +39,7 @@ learning = LearningParams(
   batch_size=256,
   loss_computation_batch_size=1024,
   gc_every=nothing,
-  learning_rate=1e-3,
+  learning_rate=2e-4,
   l2_regularization=1e-4,
   nonvalidity_penalty=1.,
   checkpoints=[1, 2])
@@ -49,11 +49,11 @@ params = Params(
   self_play=self_play,
   learning=learning,
   memory_analysis=nothing,
-  num_iters=100,
+  num_iters=80,
   ternary_rewards=true,
   mem_buffer_size=PLSchedule(
   [      0,        20],
-  [120_000, 1_000_000]))
+  [200_000, 1_000_000]))
 
 deployed_mcts = MctsParams(self_play.mcts,
   temperature=StepSchedule(cold_temperature),
@@ -64,8 +64,10 @@ benchmark = [
     Benchmark.Full(deployed_mcts),
     Benchmark.MctsRollouts(
       MctsParams(deployed_mcts, num_iters_per_turn=1000)),
-    num_games=(DEBUG ? 1 : 200)),
+    num_games=(DEBUG ? 1 : 200),
+    color_policy=CONTENDER_WHITE),
   Benchmark.Duel(
     Benchmark.Full(deployed_mcts),
-    Benchmark.MinMaxTS(depth=4),
-    num_games=(DEBUG ? 1 : 200))]
+    Benchmark.MinMaxTS(depth=4, random_ϵ=0.05),
+    num_games=(DEBUG ? 1 : 200),
+    color_policy=CONTENDER_WHITE)]
