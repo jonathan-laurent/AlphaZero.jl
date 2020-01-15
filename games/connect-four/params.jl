4133e6805178eb928675b2a10f3c104b91ed0a1b
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2020-01-14 21:55:20 -0500

    New connect-four params

--- games/connect-four/params.jl
+++ games/connect-four/params.jl
@@ -6,7 +6,7 @@ netparams = ResNetHP(
   conv_kernel_size=(3, 3),
   num_policy_head_filters=4,
   num_value_head_filters=32,
-  batch_norm_momentum=0.7)
+  batch_norm_momentum=0.2)
 
 self_play = SelfPlayParams(
   num_games=4_000,
@@ -37,12 +37,12 @@ arena = ArenaParams(
 
 learning = LearningParams(
   samples_weighing_policy=LOG_WEIGHT,
-  batch_size=512,
+  batch_size=1024,
   loss_computation_batch_size=1024,
   optimiser=CyclicNesterov(
-    lr_base=1e-2,
-    lr_high=1e-1,
-    lr_low=1e-3,
+    lr_base=5e-3,
+    lr_high=5e-2,
+    lr_low=5e-4,
     momentum_high=0.9,
     momentum_low=0.7),
   l2_regularization=1e-4,
@@ -59,7 +59,7 @@ params = Params(
     num_game_stages=4),
   mem_buffer_size=PLSchedule(
   [      0,        60],
-  [240_000, 1_800_000]))
+  [240_000, 1_200_000]))
 
 baselines = [
   Benchmark.MctsRollouts(
@@ -67,6 +67,7 @@ baselines = [
       arena.mcts,
       num_iters_per_turn=1000,
       cpuct=1.)),
+  Benchmark.MinMaxTS(depth=4, τ=0.2),
   Benchmark.MinMaxTS(depth=5, τ=0.2)]
 
 # push!(baselines, Benchmark.Solver(ϵ=0.05))
