10710a72d26bcc7c71735cc505e38383e5d3cb91
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2020-06-29 12:25:59 -0400

    Fix flame graph profiling

--- scripts/profile/flame_graph.jl
+++ scripts/profile/flame_graph.jl
@@ -2,23 +2,24 @@
 ##### Generate a flame graph for self-play using ProfileView
 #####
 
-import AlphaZero
-
-ENV["TRAINING_MODE"] = "debug"
+using AlphaZero
 
 include("../games.jl")
 GAME = get(ENV, "GAME", "connect-four")
 SelectedGame = GAME_MODULE[GAME]
 using .SelectedGame: Game, Training
 
+include("../lib/dummy_run.jl")
+
+params, _ = dummy_run_params(Training.params, Training.benchmark)
 network = Training.Network{Game}(Training.netparams)
-env = AlphaZero.Env{Game}(Training.params, network)
+env = AlphaZero.Env{Game}(params, network)
 
 using Profile
-using ProfileView
+using ProfileSVG
 
 # The neural network should not be used at iteration 0
-AlphaZero.self_play!(env, nothing) # To compile every function
+AlphaZero.self_play_step!(env, nothing) # To compile every function
 Profile.clear()
-@profile AlphaZero.self_play!(env, nothing)
-ProfileView.svgwrite("self-play-profile.svg")
+@profile AlphaZero.self_play_step!(env, nothing)
+ProfileSVG.save("self-play-profile.svg")
