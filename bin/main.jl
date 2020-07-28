63623e609da616b5051a4c8e064e7a6e52c22f27
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2020-07-28 19:16:51 -0400

    Clean bin/main.jl

--- bin/main.jl
+++ bin/main.jl
@@ -1,31 +1,18 @@
-#####
-##### Main script for the JuliaCon 2020 demo
-#####
+## Learning a connect four agent for the JuliaCon 2020 demo
 
 ENV["JULIA_CUDA_MEMORY_POOL"] = "split" # "binned" / "split"
 
-# Enables running the script on a distant machine without an X server
-ENV["GKSwstype"]="nul"
-
 using AlphaZero
-
-const DUMMY_RUN = false
-include("../scripts/lib/dummy_run.jl")
 
 include("../games/connect-four/main.jl")
 using .ConnectFour: Game, Training
 
-params, benchmark = Training.params, Training.benchmark
-if DUMMY_RUN
-  params, benchmark = dummy_run_params(params, benchmark)
-end
-
 session = Session(
   Game,
   Training.Network{Game},
-  params,
+  Training.params,
   Training.netparams,
-  benchmark=benchmark,
+  benchmark=Training.benchmark,
   dir="sessions/connect-four",
   autosave=true,
   save_intermediate=false)
