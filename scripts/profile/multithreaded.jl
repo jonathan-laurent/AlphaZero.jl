e951f32cbca7b08fb7297417e90e4cfb87667c76
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2020-09-13 13:15:42 -0400

    Add rigorous benchmark

--- scripts/profile/multithreaded.jl
+++ scripts/profile/multithreaded.jl
@@ -1,8 +1,12 @@
 #####
-##### Testing distributed self-play
+##### Testing multithreaded self-play
 #####
 
+const NUM_GAMES = 500
+
 ENV["JULIA_CUDA_MEMORY_POOL"] = "split"
+
+# ENV["ALPHAZERO_DEFAULT_DL_FRAMEWORK"] = "FLUX" # FLUX / KNET
 
 using AlphaZero
 using ProgressMeter
@@ -10,27 +14,26 @@ using ProgressMeter
 include("../../games/connect-four/main.jl")
 using .ConnectFour: Game, Training
 
-params = Training.params
-network = Training.Network{Game}(Training.netparams)
-env = AlphaZero.Env{Game}(params, network)
-duel = Training.benchmark[1]
+struct Handler
+  progress :: Progress
+  Handler(n) = new(Progress(n))
+end
+AlphaZero.Handlers.game_played(h::Handler) = next!(h.progress)
+AlphaZero.Handlers.checkpoint_game_played(h::Handler) = next!(h.progress)
 
-const sp_progress = Progress(params.self_play.num_games)
-const arena_progress = Progress(params.arena.num_games)
-const bench_progress = Progress(duel.num_games)
-
-struct Handler end
-AlphaZero.Handlers.game_played(::Handler) = next!(sp_progress)
-AlphaZero.Handlers.checkpoint_game_played(::Handler) = next!(arena_progress)
+function bench_self_play(n)
+  params = Training.params
+  params = Params(params, self_play=SelfPlayParams(params.self_play, num_games=n))
+  network = Training.Network{Game}(Training.netparams)
+  env = AlphaZero.Env{Game}(params, network)
+  return @timed AlphaZero.self_play_step!(env, Handler(n))
+end
 
 println("Running on $(Threads.nthreads()) threads.")
-
-report, t, mem, gct = @timed AlphaZero.self_play_step!(env, Handler())
-
-# (avgr, redundancy), t, mem, gct = @timed AlphaZero.evaluate_network(
-#   env.curnn, env.bestnn, env.params, Handler())
-
-# report, t, mem, gct = @timed AlphaZero.Benchmark.run(env, duel, bench_progress)
+println("Playing one game to compile everything.")
+bench_self_play(1)
+println("Starting benchmark.")
+report, t, mem, gct = bench_self_play(NUM_GAMES)
 
 println("Total time: $t")
 println("Spent in GC: $gct")
