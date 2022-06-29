390c40ec7da66f02745edfc8bc177027c41f6cc3
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-06-29 15:27:21 +0200

    Add tests on `completed_qvalues()` in SimpleMctsTests.jl

--- redesign/src/Tests/SimpleMctsTests.jl
+++ redesign/src/Tests/SimpleMctsTests.jl
@@ -19,7 +19,7 @@ function random_walk_value(; N, start_pos, rep=1000)
     return mean(oracle(env)[2] for _ in 1:rep)
 end
 
-function uniform_mcts_policy(; n=100)
+function uniform_mcts_policy_tic_tac_toe(; n=100)
     return Policy(;
         num_simulations=n,
         num_considered_actions=9,
@@ -38,7 +38,7 @@ function profile_rollout()
 end
 
 function profile_explore()
-    policy = uniform_mcts_policy()
+    policy = uniform_mcts_policy_tic_tac_toe()
     env = tictactoe_winning()
     for _ in 1:100
         explore(policy, env)
@@ -67,11 +67,17 @@ function run_mcts_tests()
         end
     end
     @testset "mcts policy" begin
-        policy = uniform_mcts_policy()
+        policy = uniform_mcts_policy_tic_tac_toe()
         env = tictactoe_winning()
         tree = explore(policy, env)
-        best = argmax(completed_qvalues(tree))
-        @test legal_action_space(env)[best] == 3
+        qvalue_list = completed_qvalues(tree)
+        best = argmax(qvalue_list)
+
+        best_move = 3
+        @test length(qvalue_list) ==
+            length(tree.children) ==
+            length(legal_action_space(env))
+        @test legal_action_space(env)[best] == best_move
     end
     @testset "mcts inferred" begin
         @test_opt target_modules = (SimpleMcts,) profile_rollout()
