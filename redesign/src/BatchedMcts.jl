132895ce6851de0f43bb01271913dfb26112a8a8
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-08-06 18:02:45 +0200

    fixup! Add usage documentation

--- redesign/src/BatchedMcts.jl
+++ redesign/src/BatchedMcts.jl
@@ -111,8 +111,8 @@ you will need to handle it in the `EnvOracle` definition.
 
 By default, `UniformTicTacToeEnvOracle`'s `transition_fn` runs on both `CPU` and `GPU`
 depending on the array type of `envs` (a.k.a GPU's `CuArray` vs classic CPU's `Array`). To
-write your own custom state evaluation or environment simulation on the appropriate device, check `EnvOracle` and its
-example `UniformTicTacToeEnvOracle`.
+write your own custom state evaluation or environment simulation on the appropriate device,
+check `EnvOracle` and its example `UniformTicTacToeEnvOracle`.
 
 TODO: This section should show examples of using the module (using jltest?). Ideally, it
 should demonstrate different settings such as:
