8109db911cb926dd30e23720ca69f040e08e4241
AndrewSpano <andrewspanopoulos@gmail.com>
2023-07-16 05:05:03 -0400

    fix: `forward` documentation fix

--- redesign/src/Networks/Network.jl
+++ redesign/src/Networks/Network.jl
@@ -59,11 +59,11 @@ Compute the forward pass of a network on a batch of inputs.
 
 Expect a `Float32` tensor `states` whose batch dimension is the last one.
 
-Return a `(P, V)` triple where:
+Return a `(v, p)` tuple where:
 
-  - `P` is a matrix of size `(num_actions, batch_size)`. It is allowed
+- `v` is a row vector of size `(1, batch_size)`
+- `P` is a matrix of size `(num_actions, batch_size)`. It is allowed
     to put weight on invalid actions (see [`evaluate`](@ref)).
-  - `V` is a row vector of size `(1, batch_size)`
 """
 function forward end
 
