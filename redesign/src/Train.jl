638ac08b9b17bef839986ef346f3b94144c22e15
AndrewSpano <andrewspanopoulos@gmail.com>
2023-08-24 15:48:42 -0400

    feat: `Train` pipeline refactor + test cases

--- redesign/src/Train.jl
+++ redesign/src/Train.jl
@@ -1,129 +1,139 @@
 module Train
 
-using Base: @kwdef
-using BenchmarkTools
 using CUDA
 using Flux
-using JLD2
 using Logging
 using Random
 using StaticArrays
 
 using ...BatchedEnvs
 using ...BatchedMcts
-using ...BatchedMctsUtilities: Policy
-using ...EnvOracles
-using ...Evaluation
+using ...BatchedMctsUtilities
 using ...LoggingUtilities
 using ...Network
 using ...ReplayBuffers
+using ...TrainUtilities
 using ...Util.Devices
-using ...Util.Devices.KernelFuns: argmax
 
-export TrainConfig, selfplay!
+export selfplay!
 
 const MCTS = BatchedMcts
 
+"""
+    step_save_reset!(config, envs, steps_counter, actions, ep_buff, rp_buff, γ=1f0)
 
-@kwdef struct TrainConfig
-    EnvCls::Type
-    env_kwargs::Dict
-    num_envs::Int
-    num_simulations::Int
-    replay_buffer_size::Int
-    mcts_value_scale::Float32
-    train_freq::Int
-    min_train_samples::Int
-    adam_lr::Float32
-    weight_decay::Float32 = 0f0
-    gradient_clip::Float32
-    batch_size::Int
-    train_epochs::Int
-    train_logfile::String
-    eval_logfile::String
-    tb_logdir::String
-    eval_fns::Vector{Function} = []
-    eval_freq::Int
-    num_steps::Int
-end
+Steps the provided actions in the environments, saves the transition data in the episode
+buffer and resets the environments that have terminated. Data of terminated episodes is
+transferred to the replay buffer.
 
-
-function init_envs(config, num_envs, device)
-    envs = [config.EnvCls(; config.env_kwargs...) for _ in 1:num_envs]
-    return DeviceArray(device)(envs)
-end
-
-
-function init_mcts(device::Device, nn::Net, config::TrainConfig) where Net <: FluxNetwork
-    oracle = neural_network_env_oracle(; nn)
-    return Policy(;
-        device,
-        oracle,
-        num_simulations=config.num_simulations,
-        value_scale=config.mcts_value_scale
-    )
-end
-
-
-function step_save_and_reset!(config, envs, actions, episode_buffer, replay_buffer, γ=1f0)
+# Arguments
+- `config`: A `TrainConfig` object specifying the training parameters/hyperparameters.
+- `envs`: The Array/CuArray containing the environments.
+- `steps_counter`: The number-of-episode-steps counter for every environment.
+- `actions`: The actions to take (step) in the environments.
+- `ep_buff`: The episode buffer to save the transition data in.
+- `rp_buff`: The replay buffer to transfer the data of terminated episodes to.
+- `γ`: The discount factor used to compute state values of terminated episodes.
+"""
+function step_save_reset!(config, envs, steps_counter, actions, ep_buff, rp_buff, γ=1f0)
+    # step
     out = act.(envs, actions)
     new_envs = first.(out)
+    steps_counter .+= 1
 
+    # save step data
     states = BatchedEnvs.vectorize_state.(envs)
     rewards = Float32.(map(x -> last(x).reward, out))
     switches = map(x -> last(x).switched, out)
-    save!(episode_buffer, states, actions, rewards, switches)
+    save!(ep_buff, states, actions, rewards, switches)
 
+    # get terminated envs
+    dones = DeviceArray(get_device(envs))(BatchedEnvs.terminated.(new_envs))
+
+    # transfer data of terminated episodes to replay buffer
+    dones_cpu = Array(dones)
     map(1:config.num_envs) do env_id
-        @inbounds done = BatchedEnvs.terminated(new_envs[env_id])
-        if done
-            # propagate rewards (for sparse-reward games)
-            # propagate_reward!(episode_buffer, env_id)  # ToDo: fix for all settings
-            compute_value_functions!(episode_buffer, env_id, γ)
+        @inbounds if dones_cpu[env_id]
+            # compute rewards-to-go (value functions)
+            compute_value_functions!(ep_buff, env_id, γ)
 
-            # reset
-            add!(replay_buffer, episode_buffer, env_id)
-            empty_env_in_buffer!(episode_buffer, env_id)
-            @inbounds new_envs[env_id] = config.EnvCls(; config.env_kwargs...)
+            # transfer sample to replay buffer and reset episode buffer
+            add!(rp_buff, ep_buff, env_id)
+            empty_env_in_buffer!(ep_buff, env_id)
+        end
+    end
+
+    # reset terminated envs
+    Devices.foreach(1:config.num_envs, get_device(envs)) do env_id
+        @inbounds if dones[env_id]
+            new_envs[env_id] = BatchedEnvs.reset(envs[env_id])
+            steps_counter[env_id] = 0
         end
     end
 
     return new_envs
 end
 
+"""
+    alphazero_loss(nn, states, actions, state_values)
 
-function alphazero_loss(nn, states, actions, rewards)
+Computes the loss of the AlphaZero network: L(θ) = L_v(θ) + L_p(θ) where
+    - L_v(θ) = (v - v_target)²   (mean squared error)
+    - L_p(θ) = -πᵀ × log(p)      (cross-entropy)
+are the value and policy losses respectively.
+
+# Arguments
+- `nn`: The neural network.
+- `states`: Environment states. Size: (state_size..., batch_size)
+- `actions`: One-hot encodings of actions taken. Size: (num_actions, batch_size)
+- `state_values`: The value function targets. Size: (1, batch_size)
+"""
+function alphazero_loss(nn, states, actions, state_values)
     pred_v, pred_logits = forward(nn, states, false)
-    value_loss = Flux.mse(pred_v, rewards)
+    value_loss = Flux.mse(pred_v, state_values)
     policy_loss = Flux.logitcrossentropy(pred_logits, actions)
     loss = value_loss + policy_loss
     return value_loss, policy_loss, loss
 end
 
+"""
+    train!(nn, replay_buffer, opt, device, config, loggers, rng)
 
-function train!(nn, replay_buffer, opt, device, config, loggers)
+Trains and returns the provided neural network on the provided replay buffer.
+
+# Arguments
+- `nn`: The neural network to train.
+- `replay_buffer`: The replay buffer containing the training data.
+- `opt`: The optimizer to use for training.
+- `device`: The device on which to train the network.
+- `config`: The configuration object containing the training parameters.
+- `loggers`: The dictionary of loggers to use for logging the training losses.
+- `rng`: The random number generator used to shuffle training data.
+"""
+function train!(nn, replay_buffer, opt, device, config, loggers, rng)
     # get the data in a meaningul format
-    states, actions, rewards = to_array(replay_buffer, device)
+    states, actions, state_values = to_array(replay_buffer)
 
     # create a dataloader
-    data = (states, actions, rewards)
-    rng = Random.MersenneTwister(0)
+    data = (states, actions, state_values)
     dataloader = Flux.DataLoader(data, batchsize=config.batch_size, shuffle=true, rng=rng)
 
     # train the model for the specified number of epochs
     for _ in 1:config.train_epochs
         for batch in dataloader
-            # if batch contains only 1 element, it will trigger NaN in BatchNorm -> skip
-            current_batch_size = size(batch[1])[end]
-            (current_batch_size == 1) && continue
+            # if the batch has only 1 element, it will trigger NaN in BatchNorm -> skip
+            (size(batch[1])[end] == 1) && continue
+
+            # convert to train device
+            batch = map(DeviceArray(device), batch)
 
             # hack: predefine variables so they get assigned correctly inside the do block
-            value_loss, policy_loss = 0f0, 0f0
+            val_loss, pol_loss = 0f0, 0f0
             loss, grads = Flux.withgradient(nn) do model
-                value_loss, policy_loss, loss = alphazero_loss(model, batch...)
+                val_loss, pol_loss, loss = alphazero_loss(model, batch...)
                 return loss
             end
-            log_losses(loggers["tb"], value_loss, policy_loss, loss)
+            "tb" in keys(loggers) && log_losses(loggers["tb"], val_loss, pol_loss, loss)
             _, nn = Flux.update!(opt, nn, grads[1])
         end
     end
@@ -131,16 +141,18 @@ function train!(nn, replay_buffer, opt, device, config, loggers)
     return nn
 end
 
+"""
+    selfplay!(config, device, nn)
 
-function save_nn(nn; step, total_steps)
-    model_state = Flux.state(Flux.cpu(nn))
-    step_str = lpad(step, length(string(total_steps)), "0")
-    jldsave("checkpoints/model_$step_str.jld2"; model_state)
-end
+Runs the AlphaZero selfplay training loop.
 
-
-function selfplay!(config, device, nn)
-
+# Arguments
+- `config`: A `TrainConfig` object specifying the training parameters/hyperparameters.
+- `device`: The device on which to run MCTS and NN training.
+- `nn`: The neural network to train.
+- `print_progress`: Whether to print progress messages in stdout.
+"""
+function selfplay!(config, device, nn, print_progress=true)
     state_size = BatchedEnvs.state_size(config.EnvCls)
     num_actions = BatchedEnvs.num_actions(config.EnvCls)
 
@@ -151,49 +163,56 @@ function selfplay!(config, device, nn)
     )
     opt = Flux.setup(adam, nn)
 
-    envs = init_envs(config, config.num_envs, device)
+    envs, steps_counter = init_envs(config, config.num_envs, device)
 
     batch_env_steps = config.num_steps ÷ config.num_envs
-    size_per_env = config.replay_buffer_size ÷ config.num_envs
     batch_train_freq = config.train_freq ÷ config.num_envs
     batch_eval_freq = config.eval_freq ÷ config.num_envs
 
-    episode_buffer = EpisodeBuffer(config.num_envs)
-    replay_buffer = ReplayBuffer(config.num_envs, size_per_env, state_size, num_actions)
+    ep_buff = EpisodeBuffer(config.num_envs, state_size)
+    rp_buff = ReplayBuffer(config.replay_buffer_size, state_size, num_actions)
 
     loggers = init_loggers(config; overwrite_logfiles=true, overwrite_tb_logdir=true)
 
-    gumbel_mcts_rng = Random.MersenneTwister(3409)
+    mcts_rng = Random.MersenneTwister(3409)
+    train_rng = Random.MersenneTwister(3409)
 
-    save_nn(nn; step=0, total_steps=batch_env_steps)
+    (config.nn_save_dir != "") && save_nn(nn, config.nn_save_dir, 0, batch_env_steps)
 
     for step in 1:batch_env_steps
-        println("Step: $step")
+        print_progress && println("Step: $step.")
 
-        # run mcts simulations to get actions
-        mcts = init_mcts(device, nn, config)
-        tree, gumbel = MCTS.gumbel_explore(mcts, envs, gumbel_mcts_rng)
-        actions = MCTS.gumbel_policy(tree, mcts, gumbel)
+        # instiantiate mcts config object
+        mcts_config = init_mcts_config(device, nn, config)
+
+        # run mcts
+        if config.use_gumbel_mcts
+            tree, gumbel = MCTS.gumbel_explore(mcts_config, envs, mcts_rng)
+            actions = MCTS.gumbel_policy(tree, mcts_config, gumbel)
+        else
+            tree = MCTS.alphazero_explore(mcts_config, envs, mcts_rng)
+            actions = MCTS.alphazero_policy(tree, mcts_config, steps_counter, mcts_rng)
+        end
 
         # step, save data from terminated episodes and reset them
-        envs .= step_save_and_reset!(config, envs, actions, episode_buffer, replay_buffer)
+        envs .= step_save_reset!(config, envs, steps_counter, actions, ep_buff, rp_buff)
 
         # train the network if it's time to do so
-        if step % batch_train_freq == 0 && length(replay_buffer) >= config.min_train_samples
-            println("Training at step: $step, len(replay_buffer): $(length(replay_buffer))")
+        if step % batch_train_freq == 0 && length(rp_buff) >= config.min_train_samples
+            print_progress && println("Training with $(length(rp_buff)) samples.")
             set_train_mode!(nn)
-            nn = train!(nn, replay_buffer, opt, device, config, loggers)
+            nn = train!(nn, rp_buff, opt, device, config, loggers, train_rng)
             set_test_mode!(nn)
-            save_nn(nn; step, total_steps=batch_env_steps)
+            (config.nn_save_dir != "") && save_nn(nn, config.nn_save_dir, step, batch_env_steps)
         end
 
         # evaluate the network if it's time to do so
-        if step % batch_eval_freq == 0 && length(config.eval_fns) > 0
+        if batch_eval_freq > 0 && step % batch_eval_freq == 0 && length(config.eval_fns) > 0
             "eval" in keys(loggers) && log_msg(loggers["eval"], "Evaluating at step: $step")
             for eval_fn in config.eval_fns
                 eval_fn(loggers, nn, config, step)
             end
-            write_msg(loggers["eval"], "\n")
+            "eval" in keys(loggers) && write_msg(loggers["eval"], "\n")
         end
     end
 end
---
+++ redesign/src/TrainUtilities.jl
@@ -0,0 +1,199 @@
+module TrainUtilities
+
+using Base: @kwdef
+using Flux
+using JLD2
+
+using ...BatchedMctsUtilities
+using ...EnvOracles
+using ...Network
+using ...Util.Devices
+
+
+export TrainConfig
+export init_envs, init_mcts_config, save_nn
+
+
+"""
+    TrainConfig(; kwargs...)
+
+Configuration for training an AlphaZero agent.
+
+# Fields
+
+## Environment variables
+- `EnvCls::Type`: The type of the environment to be used. The environment structure should
+   follow the `BatchedEnvs` interface.
+- `env_kwargs::Dict`: Keyword arguments to be passed when creating the environment instance.
+- `num_envs::Int`: The number of environments to be used for parallel simulations.
+
+Note: Environment instances are initialized like so: `env = EnvCls(; env_kwargs...)`
+
+## Common MCTS variables
+- `use_gumbel_mcts::Bool = true`: Whether to use Gumbel-MCTS instead of traditional MCTS.
+- `num_simulations::Int = 64`: Number of MCTS simulations per move.
+
+Note: The two types of implemented MCTS algorithms can be found in the following links:
+- [Gumbel-MCTS](https://openreview.net/pdf?id=bERaNdoegnO)
+- [AlphaZero-MCTS](https://arxiv.org/pdf/1712.01815.pdf)
+
+## Gumbel MCTS variables
+- `num_considered_actions::Int = 9`: Number of actions considered for selection in
+   Gumbel-MCTS.
+- `mcts_value_scale::Float32 = 0.1f0`: `c_scale` parameter for Gumbel-MCTS.
+- `mcts_max_visit_init::Int = 50`: `c_visit` parameter for Gumbel-MCTS.
+
+## AlphaZero MCTS variables
+- `c_puct::Float32 = 1.0f0`: Exploration constant for AlphaZero MCTS.
+- `alpha_dirichlet::Float32 = 0.3f0`: Alpha parameter for Dirichlet noise at root nodes.
+- `epsilon_dirichlet::Float32 = 0.25f0`: Dirichlet noise weight.
+- `tau::Float32 = 1.0f0`: Temperature parameter for action selection.
+- `collapse_tau_move::Int = 30`: Action number after which to collapse τ (tau) to 0.
+
+## NN Training variables
+- `replay_buffer_size::Int = 500`: Size of the replay buffer. It's advised to express this
+   as a multiple of the number of environments.
+- `min_train_samples::Int = 1_000`: Minimum number of samples to begin training.
+- `train_freq::Int = 500`: Frequency of neural network training. Note that this value
+   represents total environment steps, so it's best to express it as a multiple of the
+   number of environments.
+- `adam_lr::Float32 = 3e-4`: Learning rate for the Adam optimizer.
+- `weight_decay::Float32 = 0f0`: L2 regularization weight decay.
+- `gradient_clip::Float32 = 1e-3`: Gradient clipping value.
+- `batch_size::Int = 50_000`: Batch size for training.
+- `train_epochs::Int = 1`: Number of epochs per training step.
+
+## Logging variables
+- `train_logfile::String = ""`: File path to save training logs. Unused if equal to "".
+- `eval_logfile::String = ""`: File path to save evaluation logs. Unused if equal to "".
+- `tb_logdir::String = ""`: Directory for TensorBoard logs. Unused if equal to "".
+
+## NN saving dir
+- `nn_save_dir::String = ""`: Directory to save neural network model. Unused if equal to "".
+
+## Evaluation variables
+- `eval_fns::Vector{Function} = []`: Vector of evaluation functions.
+- `eval_freq::Int = 0`: Frequency of evaluation during training. No evaluation function
+   calls will take place if equal to 0.
+
+## Total train steps
+- `num_steps::Int = 1_500`: Total number of training steps. Note that this value
+   represents total environment steps, so it's best to express it as a multiple of the
+   number of environments.
+"""
+@kwdef struct TrainConfig
+    # environment variables
+    EnvCls::Type
+    env_kwargs::Dict
+    num_envs::Int
+
+    # common MCTS variables
+    use_gumbel_mcts::Bool = true
+    num_simulations::Int = 64
+
+    # Gumbel MCTS variables
+    num_considered_actions::Int = 9
+    mcts_value_scale::Float32 = 0.1f0
+    mcts_max_visit_init::Int = 50
+
+    # AlphaZero MCTS variables
+    c_puct::Float32 = 1.0f0
+    alpha_dirichlet::Float32 = 0.3f0
+    epsilon_dirichlet::Float32 = 0.25f0
+    tau::Float32 = 1.0f0
+    collapse_tau_move::Int = 30
+
+    # NN Training variables
+    replay_buffer_size::Int = 1_500
+    min_train_samples::Int = 500
+    train_freq::Int = 50
+    adam_lr::Float32 = 3e-4
+    weight_decay::Float32 = 0f0
+    gradient_clip::Float32 = 1e-3
+    batch_size::Int = 50_000
+    train_epochs::Int = 1
+
+    # Logging variables
+    train_logfile::String = ""
+    eval_logfile::String = ""
+    tb_logdir::String = ""
+
+    # NN saving dir
+    nn_save_dir::String = ""
+
+    # Evaluation variables
+    eval_fns::Vector{Function} = []
+    eval_freq::Int = 0
+
+    # Total train steps
+    num_steps::Int = 1_500
+end
+
+"""
+    init_envs(config::TrainConfig, num_envs::Int, device::Device)
+
+Initializes and returns arrays contraining the environments to be used for training and
+the number-of-episode-steps counter for every environments. Both arrays are placed on the
+`device` device.
+"""
+function init_envs(config::TrainConfig, num_envs::Int, device::Device)
+    envs = DeviceArray(device)([config.EnvCls(; config.env_kwargs...) for _ in 1:num_envs])
+    steps_counter = zeros(Int, device, num_envs)
+    return envs, steps_counter
+end
+
+"""
+    init_mcts_config(device::Device, nn::Net, config) where Net <: FluxNetwork
+
+Initializes and returns an MCTS configuration object based on the `config` configuration
+object. The `nn` neural network will be used in the environment oracle.
+
+Note: This function can be dispatched on `config` object, as it can either be of type
+`TrainConfig` or just a NamedTuple containing the important fields for the appropriate MCTS
+configuration structure. The NamedTuple version will be used during evaluation to avoid
+having to create an unnecessary `TrainConfig` object.
+"""
+function init_mcts_config(device::Device, nn::Net, config) where Net <: FluxNetwork
+    oracle = neural_network_env_oracle(; nn)
+    if config.use_gumbel_mcts
+        return GumbelMctsConfig(;
+            device,
+            oracle,
+            num_simulations=config.num_simulations,
+            num_considered_actions=config.num_considered_actions,
+            value_scale=config.mcts_value_scale,
+            max_visit_init=config.mcts_max_visit_init
+        )
+    else
+        return AlphaZeroMctsConfig(;
+            device,
+            oracle,
+            num_simulations=config.num_simulations,
+            c_puct=config.c_puct,
+            alpha_dirichlet=config.alpha_dirichlet,
+            epsilon_dirichlet=config.epsilon_dirichlet,
+            tau=config.tau,
+            collapse_tau_move=config.collapse_tau_move
+        )
+    end
+end
+
+"""
+    save_nn(nn::Net, save_dir::String, step::Int, steps::Int) where Net <: FluxNetwork
+
+Saves the `nn` neural network to the `save_dir` directory. The `step` and `steps` arguments
+are used to create a filename for the saved model. Filenames will have padded 0's in front
+to preserve the order of the files.
+
+Notes:
+- The `save_dir` directory is created if it doesn't exist.
+- The model is transferred to the CPU before saving.
+"""
+function save_nn(nn::Net, save_dir::String, step::Int, steps::Int) where Net <: FluxNetwork
+    !isdir(save_dir) && mkpath(save_dir)
+    model_state = Flux.state(Flux.cpu(nn))
+    step_str = lpad(step, length(string(steps)), "0")
+    jldsave("$save_dir/model_$step_str.jld2"; model_state)
+end
+
+end
