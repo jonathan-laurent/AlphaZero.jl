85c862c9e551cd48e92962959c1b84b247505ed5
AndrewSpano <andrewspanopoulos@gmail.com>
2023-08-24 15:48:16 -0400

    fix: `Minimax` fixes

--- redesign/src/Minimax.jl
+++ redesign/src/Minimax.jl
@@ -1,22 +1,26 @@
 module Minimax
 
+using Distributions
+using Flux
+
 using ..BatchedEnvs
 
 export zero_eval_fn
-export deterministic_minimax
+export minimax
+export stochastic_minimax
 export minimax_compute_optimal_actions
 
 
-zero_eval_fn(_, _) = 0f0
+zero_eval_fn(_, _) = 0.0
+amplify(r) = iszero(r) ? r : Inf * sign(r)
 
 function get_legal_actions(env)
     na = BatchedEnvs.num_actions(typeof(env))
     return [Int16(a) for a in 1:na if BatchedEnvs.valid_action(env, a)]
 end
 
-
 """
-    deterministic_max_value(env, depth, eval_fn)
+    max_value(env, depth, eval_fn, amplify_rewards, γ)
 
 Applies the maximizing part of the minimax algorithm to find the best action.
 It accepts the current environment `env`, the current depth `depth` and an
@@ -25,12 +29,16 @@ when 0 depth is reached.
 In most 2-player environments, we assume by default that the maximizing player
 is the first player (usually white/cross).
 """
-function deterministic_max_value(env, depth, eval_fn)
-    (depth == 0) && return (eval_fn(env, true), 0, 0)
+function max_value(env, depth, eval_fn, amplify_rewards, γ)
+    if depth == 0
+        val = eval_fn(env, true)
+        return (fill(val, BatchedEnvs.num_actions(typeof(env))), val, 0, 0)
+    end
 
     legal_actions = get_legal_actions(env)
 
-    best_score = -Inf32
+    scores = fill(-Inf, BatchedEnvs.num_actions(typeof(env)))
+    best_score = -Inf
     best_action = legal_actions[1]
     best_depth = -1
 
@@ -38,25 +46,26 @@ function deterministic_max_value(env, depth, eval_fn)
         newenv, info = BatchedEnvs.act(env, action)
 
         if BatchedEnvs.terminated(newenv)
-            value = 0f0
-            score_depth = depth
+            value = 0.0
+            depth_val = depth
         else
-            recursive_fn = info.switched ? deterministic_min_value : deterministic_max_value
-            value, _, score_depth = recursive_fn(newenv, depth - 1, eval_fn)
+            fn = info.switched ? min_value : max_value
+            _, value, _, depth_val = fn(newenv, depth - 1, eval_fn, amplify_rewards, γ)
         end
-        score = value + info.reward
+        reward = amplify_rewards ? amplify(info.reward) : info.reward
+        score = γ * value + reward
+        scores[action] = score
 
-        if score > best_score || (score == best_score && score_depth > best_depth)
-            best_score, best_action, best_depth = score, action, score_depth
+        if score > best_score || (score == best_score && depth_val > best_depth)
+            best_score, best_action, best_depth = score, action, depth_val
         end
     end
 
-    return best_score, best_action, best_depth
+    return scores, best_score, best_action, best_depth
 end
 
-
 """
-    deterministic_min_value(env, depth, eval_fn)
+    min_value(env, depth, eval_fn, amplify_rewards, γ)
 
 Applies the minimizing part of the minimax algorithm to find the best action.
 It accepts the current environment `env`, the current depth `depth` and an
@@ -65,12 +74,16 @@ when 0 depth is reached.
 In most 2-player environments, we assume by default that the minimizing player
 is the second player (usually black/nought).
 """
-function deterministic_min_value(env, depth, eval_fn)
-    (depth == 0) && return (eval_fn(env, false), 0, 0)
+function min_value(env, depth, eval_fn, amplify_rewards, γ)
+    if depth == 0
+        val = eval_fn(env, false)
+        return (fill(val, BatchedEnvs.num_actions(typeof(env))), val, 0, 0)
+    end
 
     legal_actions = get_legal_actions(env)
 
-    best_score = Inf32
+    scores = fill(Inf, BatchedEnvs.num_actions(typeof(env)))
+    best_score = Inf
     best_action = legal_actions[1]
     best_depth = -1
 
@@ -78,32 +91,45 @@ function deterministic_min_value(env, depth, eval_fn)
         newenv, info = BatchedEnvs.act(env, action)
 
         if BatchedEnvs.terminated(newenv)
-            value = 0f0
-            score_depth = depth
+            value = 0.0
+            depth_val = depth
         else
-            recursive_fn = info.switched ? deterministic_max_value : deterministic_min_value
-            value, _, score_depth = recursive_fn(newenv, depth - 1, eval_fn)
+            fn = info.switched ? max_value : min_value
+            _, value, _, depth_val = fn(newenv, depth - 1, eval_fn, amplify_rewards, γ)
         end
         min_perspective_reward = -info.reward
-        score = value + min_perspective_reward
+        reward = amplify_rewards ? amplify(min_perspective_reward) : min_perspective_reward
+        score = γ* value + reward
+        scores[action] = score
 
-        if score < best_score || (score == best_score && score_depth > best_depth)
-            best_score, best_action, best_depth = score, action, score_depth
+        if score < best_score || (score == best_score && depth_val > best_depth)
+            best_score, best_action, best_depth = score, action, depth_val
         end
     end
 
-    return best_score, best_action, best_depth
+    return scores, best_score, best_action, best_depth
 end
 
-
 """
-    deterministic_minimax(env, depth=5, eval_fn=zero_eval_fn; is_maximizing=true)
+    minimax(
+        env;
+        depth=5,
+        eval_fn=zero_eval_fn,
+        is_maximizing=true,
+        amplify_rewards=false,
+        γ=1.0
+    )
 
 Applies the minimax algorithm to find the best action for the current player.
 It accepts the current environment `env`, the current depth `depth` and an
 evaluation function `eval_fn` that evaluates the current state of the game
-when 0 depth is reached. The algorithm works in a deterministic way, by selecting
-always the action with the best score achieved in the closest (ro the root) possible depth.
+when 0 depth is reached. If `amplify_rewards` is set to true, environment rewards
+will be multipled by a large constant. This can be useful in cases where heuristic
+functions might give higher scores than the rewards-to-go. The `γ` parameter is
+the discount factor used to compute the action values.
+
+The algorithm works in a deterministic way, by selecting always the action with the
+best score achieved in the closest (to the root) possible depth.
 
 The evaluation function is heuristic and should have the following format:
 
@@ -112,15 +138,84 @@ The evaluation function is heuristic and should have the following format:
 where `env` is an environment following the `BatchedEnvs` interface and `player`
 is a boolean indicating whether the current player is the maximizing player.
 """
-function deterministic_minimax(env; depth=5, eval_fn=zero_eval_fn, is_maximizing=true)
-    fn = is_maximizing ? deterministic_max_value : deterministic_min_value
-    _, a, _ = fn(env, depth, eval_fn)
+function minimax(
+    env;
+    depth=5,
+    eval_fn=zero_eval_fn,
+    is_maximizing=true,
+    amplify_rewards=false,
+    γ=1.0
+)
+    recursive_fn = is_maximizing ? max_value : min_value
+    _, _, a, _ = recursive_fn(env, depth, eval_fn, amplify_rewards, γ)
     return a
 end
 
+"""
+    sample_action(scores, rng)
 
+Samples an action from the scores vector, which is assumed to be a vector containing
+a score (q value) for each action. The probabilities of the scores are computed using
+a softmax function, and then a sample is drawn the categorical distribution defined
+by the probabilities of the scores.
+"""
+function sample_action(scores, env, rng)
+    # if all entries are -Inf, just set the probs to be uniform of valid actions
+    if all(isinf.(scores))
+        valid_actions = get_legal_actions(env)
+        scores[scores .== -Inf] .= 0
+        scores[valid_actions] .= 1 / length(valid_actions)
+        probs = scores
+    # else, just apply softmax to scores
+    else
+        probs = Flux.softmax(scores)
+    end
+    return rand(rng, Distributions.Categorical(probs))
+end
 
+"""
+    stochastic_minimax(
+        env,
+        rng;
+        depth=5,
+        eval_fn=zero_eval_fn,
+        is_maximizing=true,
+        amplify_rewards=false,
+        γ=1.0
+    )
 
+Applies the minimax algorithm to find the next action for the current player.
+Unlike minimax, at the root node the action is not selected deterministically
+(argmax/argmin), but rather it samples from a categorical distribution of the scores.
+"""
+function stochastic_minimax(
+    env,
+    rng;
+    depth=5,
+    eval_fn=zero_eval_fn,
+    is_maximizing=true,
+    amplify_rewards=false,
+    γ=1.0
+)
+    recursive_fn = is_maximizing ? max_value : min_value
+    scores, _, _, _ = recursive_fn(env, depth, eval_fn, amplify_rewards, γ)
+    (!is_maximizing) && (scores .*= -1)
+    return sample_action(scores, env, rng)
+end
+
+"""
+    max_value_store_optimal_actions!(
+        env,
+        depth,
+        eval_fn,
+        optimal_policy_dict,
+        possible_envs
+    )
+
+Applies the maximizing part of the minimax algorithm to find the best action, while
+storing the optimal policy in a dictionary, as well as new environments encountered.
+It returns the best score and the best action.
+"""
 function max_value_store_optimal_actions!(
     env,
     depth,
@@ -167,7 +262,19 @@ function max_value_store_optimal_actions!(
     return best_score, best_action
 end
 
+"""
+    min_value_store_optimal_actions!(
+        env,
+        depth,
+        eval_fn,
+        optimal_policy_dict,
+        possible_envs
+    )
 
+Applies the minimizing part of the minimax algorithm to find the best action, while
+storing the optimal policy in a dictionary, as well as new environments encountered.
+It returns the best score and the best action.
+"""
 function min_value_store_optimal_actions!(
     env,
     depth,
@@ -215,7 +322,19 @@ function min_value_store_optimal_actions!(
     return best_score, best_action
 end
 
+"""
+    minimax_compute_optimal_actions(
+        env;
+        depth=5,
+        eval_fn=zero_eval_fn,
+        is_maximizing=true
+    )
 
+Applies the minimax algorithm to find the best action for the current player, while
+storing the optimal policy in a dictionary, as well as new environments encountered.
+It returns the best action, the optimal policy dictionary and the list of possible
+environments.
+"""
 function minimax_compute_optimal_actions(
     env;
     depth=5,
--- redesign/src/Tests/MinimaxTests.jl
+++ redesign/src/Tests/MinimaxTests.jl
@@ -1,33 +1,37 @@
 module MinimaxTests
 
+using Random
 using Test
 
 using ..Common.BitwiseTicTacToe
 using ..Common.BitwiseTicTacToeHeuristic
+using ..Common.BitwiseConnectFour
+using ..Common.BitwiseConnectFourHeuristic
 using ...BatchedEnvs
 using ...Minimax
 
 export run_minimax_tests
 
 function run_minimax_tests()
-    @testset "TicTacToe" test_tictactoe_minimax()
+    @testset "Deterministic Minimax" test_deterministic_minimax()
+    @testset "Stochastic Minimax" test_stochastic_minimax()
 end
 
-function test_tictactoe_minimax()
-    @testset "Cross player winning" begin
+function test_deterministic_minimax()
+    @testset "TicTacToe: Cross player winning" begin
         env = BitwiseTicTacToeEnv()
         env, _ = BatchedEnvs.act(env, 1)
         env, _ = BatchedEnvs.act(env, 2)
         env, _ = BatchedEnvs.act(env, 5)
         env, _ = BatchedEnvs.act(env, 8)
 
-        a = deterministic_minimax(env; depth=5, eval_fn=zero_eval_fn, is_maximizing=true)
+        a = minimax(env; depth=5, eval_fn=zero_eval_fn, is_maximizing=true)
 
         # best action should be for cross player to win along main diagonal
         @test a == 9
     end
 
-    @testset "Nought player winning" begin
+    @testset "TicTacToe: Nought player winning" begin
         env = BitwiseTicTacToeEnv()
         env, _ = BatchedEnvs.act(env, 2)
         env, _ = BatchedEnvs.act(env, 5)
@@ -35,33 +39,52 @@ function test_tictactoe_minimax()
         env, _ = BatchedEnvs.act(env, 1)
         env, _ = BatchedEnvs.act(env, 9)
 
-        a = deterministic_minimax(env; depth=5, eval_fn=zero_eval_fn, is_maximizing=false)
+        a = minimax(env; depth=5, eval_fn=zero_eval_fn, is_maximizing=false)
 
         # best action should be for nought player to play bottom left
         @test a == 3
     end
 
-    @testset "Drawn game" begin
+    @testset "TicTacToe: Early pos" begin
         env = BitwiseTicTacToeEnv()
+        env, _ = BatchedEnvs.act(env, 1)
 
-        a = deterministic_minimax(env; depth=9, eval_fn=zero_eval_fn, is_maximizing=true)
+        a = minimax(env; depth=9, eval_fn=zero_eval_fn, is_maximizing=false)
 
-        # best action should be for nought player to play bottom left
-        @test a in [1, 3, 5, 7, 9]
+        # best action should be for nought player to in the middle
+        @test a == 5
     end
 
-    @testset "Cross player winning with small depth + heuristic" begin
+    @testset "TicTacToe: Cross player winning with small depth + heuristic" begin
         env = BitwiseTicTacToeEnv()
         env, _ = BatchedEnvs.act(env, 5)
         env, _ = BatchedEnvs.act(env, 4)
         env, _ = BatchedEnvs.act(env, 7)
         env, _ = BatchedEnvs.act(env, 3)
 
-        a = deterministic_minimax(env; depth=2, eval_fn=tictactoe_eval_fn,
-                                  is_maximizing=true)
+        a = minimax(env; depth=2, eval_fn=tictactoe_eval_fn, is_maximizing=true)
 
         # best action should be for cross player to play on the rightmost column
         @test a in [8, 9]
+    end
+end
+
+function test_stochastic_minimax()
+    @testset "Connect-4: Cross player winning" begin
+        env = BitwiseConnectFourEnv()
+        env, _ = BatchedEnvs.act(env, 4)
+        env, _ = BatchedEnvs.act(env, 1)
+        env, _ = BatchedEnvs.act(env, 3)
+        env, _ = BatchedEnvs.act(env, 1)
+        env, _ = BatchedEnvs.act(env, 6)
+        env, _ = BatchedEnvs.act(env, 1)
+
+        rng = Random.MersenneTwister(0)
+
+        a = stochastic_minimax(env, rng; depth=5, is_maximizing=true, amplify_rewards=true)
+
+        # when rewards are amplified, only the best action should be chosen
+        @test a == 5
     end
 end
 
